67366df48ea6674cdecfaaad4db449a479e4984a
someXXYY <betaXXX@gmail.com>
2026-01-26 22:18:20 +0800

    error: catching polymorphic type ‘class bustub::TransactionAbortException’ by value [-Werror=catch-value=]

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -54,7 +54,7 @@ auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
           if (!is_locked) {
             throw ExecutionException("IndexScan Executor Get Table Lock Failed");
           }
-        } catch (TransactionAbortException e) {
+        } catch (const TransactionAbortException &e) {
           throw ExecutionException("IndexScan Executor Get Row Lock Failed");
         }
       }
